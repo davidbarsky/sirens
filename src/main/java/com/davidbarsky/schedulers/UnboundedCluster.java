62f9830b8ecd6cfdf33ce68d9d30fc717a22f2aa
David Barsky <me@davidbarsky.com>
2016-12-04 14:31:30 -0500

    [WIP] Implemented structure of linear clustering, still need to implement longest path finder.

--- src/main/java/com/davidbarsky/schedulers/UnboundedCluster.java
+++ src/main/java/com/davidbarsky/schedulers/UnboundedCluster.java
@@ -1,72 +1,48 @@
 package com.davidbarsky.schedulers;
 
 import com.davidbarsky.dag.DAGGenerator;
+import com.davidbarsky.dag.TopologicalSorter;
 import com.davidbarsky.dag.models.Task;
 import com.davidbarsky.dag.models.TaskQueue;
 import com.davidbarsky.dag.models.states.MachineType;
 import info.rmarcus.ggen4j.graph.Vertex;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class UnboundedCluster {
-    public static void linearCluster2(int numQueues) {
-        List<TaskQueue> queues = new ArrayList<>();
-        Map<Integer, Task> tasks = new HashMap<>();
+    // The goal is to find the longest critical path, and place it onto
+    // a processor or machine. We'll track visitation state using a set.
+    public List<TaskQueue> linearCluster(int numQueues) {
+        Collection<Vertex> graph = DAGGenerator.getErdosGNMSources();
+        TaskQueue linearizedDag = TopologicalSorter.invoke(graph);
 
-        IntStream.range(0, numQueues)
-                .forEach(k -> queues.add(new TaskQueue(MachineType.SMALL)));
-        Collection<Vertex> vertices = DAGGenerator.getErdosGNMSources();
+        List<List<Task>> paths = longestPath(linearizedDag.getTasks());
+        List<TaskQueue> result = new ArrayList<>();
+        for (List<Task> path : paths) {
+            result.add(new TaskQueue(MachineType.SMALL, path));
+        }
+
+        return result;
     }
 
+    // Returns a list of the longest paths
+    public List<List<Task>> longestPath(List<Task> linearizedDAG) {
+        return longestPathHelper(linearizedDAG,
+                                new HashSet<>(),
+                                new ArrayList<>());
+    }
 
-    public static List<List<Task>> linearCluster(List<TaskQueue> taskQueues) {
-        Set<Task> visited = new HashSet<>();
-        List<List<Task>> criticalPaths = new ArrayList<>();
+    private List<List<Task>> longestPathHelper(List<Task> linearizedDag,
+                                               Set<Task> visited,
+                                               List<Task> path) {
+        Task current = linearizedDag.get(0);
+        visited.add(current);
+        path.add(current);
 
-        for (TaskQueue taskQueue : taskQueues) {
-            Queue<Task> queue = new LinkedList<>();
-            Task head = taskQueue.getTasks().get(0);
-            List<Task> path = new ArrayList<>();
-
-            queue.add(head);
-            while (!queue.isEmpty()) {
-                Task current = queue.remove();
-                for (Task neighbor : current.getDependents().keySet()) {
-                    if (!visited.contains(neighbor)) {
-                        queue.add(neighbor);
-                        path.add(neighbor);
-                        visited.add(neighbor);
-                    }
-                }
+        for (Task child : current.getDependents().keySet()) {
+            if (!visited.contains(child) && !path.contains(child)) {
+                return longestPathHelper(linearizedDag, visited, path);
             }
-
-            criticalPaths.add(path);
         }
-
-        return criticalPaths;
-    }
-
-    public List<Task> edgeZero(List<TaskQueue> taskQueues) {
-        ArrayList<Task> tasks = flattenTaskQueues(taskQueues);
-        LinkedHashSet<Task> visited = new LinkedHashSet<>();
-
-        while (tasks.size() != visited.size()) {
-            Task highestNetworkCost = tasks.get(0);
-            visited.add(highestNetworkCost);
-            highestNetworkCost.getDependents().replace(highestNetworkCost, 0);
-        }
-
-        return visited.stream().collect(Collectors.toList());
-    }
-
-    private ArrayList<Task> flattenTaskQueues(List<TaskQueue> taskQueues) {
-        ArrayList<Task> tasks = new ArrayList<>();
-        taskQueues.forEach(tq -> {
-            tasks.addAll(tq.getTasks());
-        });
-        Collections.sort(tasks, Comparator.comparingInt(t -> t.getDependents().get(t)));
-        return tasks;
     }
 }
