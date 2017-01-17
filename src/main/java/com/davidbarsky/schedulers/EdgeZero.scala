f8d7c8b99e3068732e4ef87f6b3e2fbeca3df068
David Barsky <me@davidbarsky.com>
2017-01-17 13:28:07 -0500

    Refactored EdgeZero into GraphProperties, now calculates critical paths and alap

--- src/main/java/com/davidbarsky/schedulers/EdgeZero.scala
+++ src/main/java/com/davidbarsky/schedulers/EdgeZero.scala
@@ -1,61 +1,28 @@
 package com.davidbarsky.schedulers
 
 import java.util
+import java.util.Comparator
+import java.util.stream.Collectors
 
-import collection.JavaConverters._
-import collection.mutable
 import com.davidbarsky.dag.TopologicalSorter
 import com.davidbarsky.dag.models.{Task, TaskQueue}
-import info.rmarcus.ggen4j.graph.Vertex
 
 // Since the EZ algorithm considers only the communication costs among nodes
 // to make scheduling decisions, it does not guarantee optimal schedules
 // for both fork and join structures.
 class EdgeZero extends UnboundedScheduler {
   override def generateSchedule(numNodes: Int): util.List[TaskQueue] = {
+    // descending order of edge weight
+    val byEdgeWeight: Comparator[Task] = (t1: Task, t2: Task) =>
+      Integer.compare(t2.edgeWeight(), t1.edgeWeight())
 
-    // We generate the graph, and sort it by the node's edge weight
-    // in descending order.
-    val graph: util.List[Task] = TopologicalSorter.generateGraph(numNodes)
-
-    val visited = mutable.Set[Task]()
-    for (task: Task <- graph.asScala) {
-
-    }
+    val graph: util.List[Task] = TopologicalSorter
+      .generateGraph(numNodes)
+      .stream()
+      .sorted(byEdgeWeight)
+      .collect(Collectors.toList[Task])
+    val examinedTasks = new util.HashSet[Task]()
 
     new util.ArrayList[TaskQueue]()
-  }
-
-  // So far, cost of all tasks is either 1 or 2, depending if they're scheduled
-  // on a small/large machine. Since we don't know the *cost* of a task until its scheduled
-  // on a machine, tLevel  & bLevel will assume MachineType.SMALL, with a default value of 1.
-  def findTLevel(graph: util.List[Task], computationCost: Int = 1): util.Map[Task, Integer] = {
-    val levels = new util.HashMap[Task, Integer]()
-    for (task: Task <- graph.asScala) {
-      var max = 0
-      for (parent: Task <- task.getDependencies.keySet().asScala) {
-        if (levels.getOrDefault(parent, 0) + computationCost + parent.getCostTo(task) > max) {
-          max = levels.getOrDefault(parent, 0) + computationCost + parent.getCostTo(task)
-        }
-      }
-      levels.put(task, max)
-    }
-
-    levels
-  }
-
-  def findBLevel(graph: util.List[Task], computationCost: Int = 1): util.Map[Task, Integer] = {
-    val levels = new util.HashMap[Task, Integer]()
-    for (task: Task <- graph.asScala) {
-      var max = 0
-      for (child: Task <- task.getDependents.keySet().asScala) {
-        if (child.getCostTo(task) + levels.getOrDefault(task, 0) > max) {
-          max = levels.getOrDefault(child, 0) + child.getCostTo(task)
-        }
-      }
-      levels.put(task, max + computationCost)
-    }
-
-    levels
   }
 }
--- src/test/java/com/davidbarsky/schedulers/EdgeZeroTest.java
+++ src/test/java/com/davidbarsky/schedulers/EdgeZeroTest.java
@@ -1,53 +1,10 @@
 package com.davidbarsky.schedulers;
 
-import com.davidbarsky.dag.TopologicalSorter;
-import com.davidbarsky.dag.models.Task;
-import org.junit.Before;
 import org.junit.Test;
-
-import java.util.List;
-import java.util.Map;
 
 import static org.junit.Assert.*;
 
 public class EdgeZeroTest {
-    private List<Task> graph;
-    private EdgeZero scheduler;
-
-    @Before
-    public void init() {
-        graph = TopologicalSorter.generateGraph(20);
-        scheduler = new EdgeZero();
-    }
-
-    @Test
-    public void findBLevel() throws Exception {
-        Map<Task, Integer> leveledGraph = scheduler.findBLevel(graph, 1);
-        // Sanity Checks
-        leveledGraph.forEach((task, integer) -> {
-            assertNotNull(integer);
-            assertNotNull(task);
-        });
-
-        // B-levels must consider computation costs, so zero-values are *not* possible.
-        boolean noZeroValuesPresent = leveledGraph.values().stream()
-                .noneMatch(i -> i == 0);
-        assertTrue(noZeroValuesPresent);
-    }
-
-    @Test
-    public void findTLevel() throws Exception {
-        Map<Task, Integer> leveledGraph = scheduler.findTLevel(graph, 1);
-        // Sanity Checks
-        leveledGraph.forEach((task, integer) -> {
-            assertNotNull(integer);
-            assertNotNull(task);
-        });
-
-        boolean someZeroValuesPresent = leveledGraph.values().stream()
-                .anyMatch(i -> i == 0);
-        assertTrue(someZeroValuesPresent);
-    }
 
     @Test
     public void generateSchedule() throws Exception {
