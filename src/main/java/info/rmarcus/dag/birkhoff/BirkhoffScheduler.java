a63408ed6f0107525e952cd1f0133daa88a529ac
Ryan Marcus <ryan@rmarcus.info>
2017-01-26 19:27:22 -0500

    precond

--- src/main/java/info/rmarcus/dag/birkhoff/BirkhoffScheduler.java
+++ src/main/java/info/rmarcus/dag/birkhoff/BirkhoffScheduler.java
@@ -1,32 +1,37 @@
 package info.rmarcus.dag.birkhoff;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.ToDoubleFunction;
+
 import com.davidbarsky.dag.CostAnalyzer;
 import com.davidbarsky.dag.DAGGenerator;
 import com.davidbarsky.dag.models.Task;
 import com.davidbarsky.dag.models.TaskQueue;
+
 import info.rmarcus.birkhoffvonneumann.CoeffAndMatrix;
+import info.rmarcus.birkhoffvonneumann.MatrixUtils;
+import info.rmarcus.birkhoffvonneumann.exceptions.BVNException;
 import info.rmarcus.birkhoffvonneumann.learners.generalized_loss.MHJointPermutationLearner;
-import info.rmarcus.dag.cca.CCAScheduler;
 import info.rmarcus.dag.birkhoff.permsolve.PermutationSolver;
+import info.rmarcus.dag.cca.CCAScheduler;
 import info.rmarcus.dag.sla.SLA;
 import info.rmarcus.dag.sla.TotalLatencySLA;
 import info.rmarcus.ggen4j.graph.Vertex;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.function.ToDoubleFunction;
 
 public class BirkhoffScheduler {
 
 	private List<Task> tasks;
 	private MHJointPermutationLearner jps;
 
-	private final static int DEADLINE = 2500;
+	private final static int DEADLINE = 2000;
 
 
 	public void measure(int n) {
-		Collection<Vertex> vertices = DAGGenerator.getSparseLU(n);
+		List<Vertex> vertices = new ArrayList<>(DAGGenerator.getSparseLU(n));
 		tasks = DAGGenerator.verticesToTasks(vertices);
 				
 		System.out.println("Number of tasks: " + tasks.size());
@@ -46,14 +51,44 @@ public class BirkhoffScheduler {
 		System.out.println(tqs);
 		sla.printBreakdown((List<TaskQueue>) tqs);
 		System.out.println("Done!");
+		
+		// build the topo sort preconditioning permutation
+		int[] topoPre = new int[vertices.size()];
+		for (int i = 0; i < vertices.size(); i++) {
+			topoPre[i] = vertices.get(i).getID();
+		}
+		
+		// build the schedule preconditioning premutation
+		// sort the collection of taskqueues by ID of the first element, descending
+		List<TaskQueue> schedule = new ArrayList<>(tqs);
+		Comparator<TaskQueue> comp = ((a, b) -> b.getTasks().get(0).getID() - a.getTasks().get(0).getID());
+		Collections.sort(schedule, comp);
+		
+		// and now build the permutation
+		List<Task> flattenedSchedule = new ArrayList<>(vertices.size());
+		schedule.forEach(tq -> flattenedSchedule.addAll(tq.getTasks()));
+		
+		int[] schedPre = new int[vertices.size()];
+		for (int i = 0; i < schedPre.length; i++) {
+			schedPre[i] = flattenedSchedule.get(i).getID();
+		}
 
 		ToDoubleFunction<List<double[][]>> loss = slaLoss(sla);
 
 		jps = new MHJointPermutationLearner(new int[] { tasks.size(), tasks.size() }, loss);
+		
+		try {
+			jps.precondition(0, MatrixUtils.preconditionedBistoch(schedPre, 0.4));
+			jps.precondition(1, MatrixUtils.preconditionedBistoch(topoPre, 0.0));
+		} catch (BVNException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		
 		for (int i = 0; i < 1000000; i++) {
 			if (i % 1000 == 0)
 				System.out.println(i);
-			jps.iterate(1);
+			jps.iterate(0);
 		}
 
 		List<double[][]> best = jps.getBest();
@@ -98,7 +133,7 @@ public class BirkhoffScheduler {
 	public static void main(String[] args) {
 		BirkhoffScheduler bs = new BirkhoffScheduler();
 		long t = System.currentTimeMillis();
-		bs.measure(8);
+		bs.measure(10);
 		//bs.measure(18);
 		//System.out.println(bs.getBest());
 		System.out.println(System.currentTimeMillis() - t);
